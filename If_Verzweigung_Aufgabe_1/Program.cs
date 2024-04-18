4f99f741e7adbcfa5bbd3af9fe543ee7eca35e01
Alexander Chrebtow <AlexChrebtow.2000@gmail.com>
2024-04-18 12:33:23 +0200

    Uebungsaufgabe if-Verzweigung

---
+++ If_Verzweigung_Aufgabe_1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace If_Verzweigung_Aufgabe_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bitte geben Sie Ihren Nutzernamen ein: ");
+            string nutzername = Console.ReadLine();
+
+            if (nutzername == "Peter")
+            {
+                Console.WriteLine("Herzlich Willkommen!");
+
+            }
+            else {
+                Console.WriteLine("Sie haben keine Berechtigung zur Nutzung des Programms.");
+            }
+        }
+    }
+}
